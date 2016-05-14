75a7270db382eb43cca11073aafa515f7a9df583
Fede Prado <fpradomacat@hotmail.com>
2016-05-14 19:46:53 -0300

    Agrego DAO's
    Estaban en otra carpeta

---
+++ Proyecto/TPFINAL_DAOs/StringConexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAOs
+{
+    public static class StringConexion
+    {
+        public static string StringBD
+        {
+            get
+            {
+                return @"Data Source=FEDECARP-PC\SQLSERVER;Initial Catalog=ProyectoWeb;Integrated Security=True";
+            }
+            set
+            {
+            }
+        }
+    }
+}
